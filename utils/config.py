14838d6c20f7a5c2753207b33b68459a0699a3e5
Daniil Flask <78703851+notflask@users.noreply.github.com>
2022-06-17 00:07:55 +0200

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,4 @@
-token = "7b570e3e5869b6290e3a85d91c26e942"
+token = "<your open weather map token>"
 url = (
     "https://api.openweathermap.org/data/2.5/weather?"
     "lat={latitude}&lon={longitude}&"
